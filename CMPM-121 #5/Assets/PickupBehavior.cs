1195d964fb9f231d9e86c4c98c7eab1ba6dee734
Carlos Cisneros <33742776+Ccisneros910@users.noreply.github.com>
2019-02-19 12:00:07 -0800

    Initial file commit

---
+++ CMPM-121 #5/Assets/PickupBehavior.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupBehavior : MonoBehaviour
+{
+    public Rigidbody player;
+    private ParticleSystem effect;
+
+    //Start is called before the first frame update
+    void Start()
+    {
+        effect = GetComponent<ParticleSystem>();
+        effect.Stop();
+    }
+    private IEnumerator OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            effect.Play();
+            yield return new WaitForSeconds(.5f);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ CMPM-121 #5/Assets/double_door_open.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class double_door_open : MonoBehaviour
+{
+    private BoxCollider trigger;
+    private Animator open;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        trigger = GetComponent<BoxCollider>();
+        open = GetComponent<Animator>();
+        open.Play("door_1_closed", 0, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "Player")
+        {
+            open.Play("door_1_open",0, 0);
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.name == "Player")
+        {
+            open.Play("door_1_opened", 0, 0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
