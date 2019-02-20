993aa85c225ce1465c0fdc7cb76b0e0ff59de26d
Carlos Cisneros <33742776+Ccisneros910@users.noreply.github.com>
2019-02-19 23:55:27 -0800

    Menu added
    A menu that will take the player to the game if the "PLAY" button is pressed has been added.

---
+++ CMPM-121 #5/Assets/MenuButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuButton : MonoBehaviour
+{
+    public Button playButton;
+    public Camera playerCamera;
+    public Camera menuCamera;
+    public Canvas menu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playButton.onClick.AddListener(ChangeCameras);
+    }
+    void ChangeCameras()
+    {
+        menuCamera.enabled = false;
+        playerCamera.enabled = true;
+        menu.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
